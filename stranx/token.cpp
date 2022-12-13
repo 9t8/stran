1b19fd393039fd4a268b681c263bbc79d9b9cbcc
9t8 <63334933+9t8@users.noreply.github.com>
2022-12-13 15:45:47 -0800

    more lambdas

--- stranx/token.cpp
+++ stranx/token.cpp
@@ -1,16 +1,16 @@
 #include "token.h"
 
-const std::type_info &get_next_token_type(token_list &tokens) {
-	assert(!tokens.empty() && "expected a token but none found (too many opening parens?)");
-
-	token &next_token(*tokens.front());
-	return typeid(next_token);
-}
-
 p_datum begin_list::parse(token_list &tokens) const {
 	tokens.pop_front();
 
-	if (get_next_token_type(tokens) == typeid(end_list)) {
+	auto get_next_token_type([&]() -> const std::type_info & {
+		assert(!tokens.empty() && "expected a token but none found (too many opening parens?)");
+
+		token &next_token(*tokens.front());
+		return typeid(next_token);
+	});
+
+	if (get_next_token_type() == typeid(end_list)) {
 		tokens.pop_front();
 		return std::make_shared<empty_list>();
 	}
@@ -19,7 +19,9 @@ p_datum begin_list::parse(token_list &tokens) const {
 	start->car = tokens.front()->parse(tokens);
 
 	std::shared_ptr<pair> p(start);
-	while (get_next_token_type(tokens) != typeid(end_list)) {
+	while (get_next_token_type() != typeid(end_list)) {
+		if (get_next_token_type() == typeid(dot)) {
+		}
 		std::shared_ptr<pair> p_new(std::make_shared<pair>());
 		p_new->car = tokens.front()->parse(tokens);
 		p->cdr = p_new;
