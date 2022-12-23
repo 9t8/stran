2dccf11d5b053bdc2f9760ff05f345b10424a069
9t8 <63334933+9t8@users.noreply.github.com>
2022-12-23 00:18:08 -0800

    refactored parsing

--- stranx/stranx.cpp
+++ stranx/stranx.cpp
@@ -3,11 +3,57 @@
 
 #include <iostream>
 
+p_datum parse_next(const token_list &tokens, size_t &idx) {
+	static auto get_next_token_type([&]() -> const std::type_info & {
+		assert(idx < tokens.size() &&
+		"expected a token but none found (too many opening parens?)");
+
+		object &next_token(*tokens[idx]);
+		return typeid(next_token);
+	});
+
+	const std::type_info &next_token_type(get_next_token_type());
+
+	++idx;
+
+	if (next_token_type == typeid(atom)) {
+		return dynamic_cast<const atom &>(*tokens[idx - 1]).val;
+	}
+
+	if (next_token_type == typeid(begin_list)) {
+		if (get_next_token_type() == typeid(end_list)) {
+			++idx;
+			return std::make_shared<empty_list>();
+		}
+
+		std::shared_ptr<pair> p(std::make_shared<pair>(parse_next(tokens, idx)));
+		p_datum start(p);
+		while (get_next_token_type() != typeid(end_list)) {
+			if (get_next_token_type() == typeid(dot)) {
+				++idx;
+				p->cdr = parse_next(tokens, idx);
+				assert(get_next_token_type() == typeid(end_list) &&
+					   "malformed improper list (misplaced dot token)");
+				break;
+			}
+			std::shared_ptr<pair> p_new(std::make_shared<pair>(parse_next(tokens, idx)));
+			p->cdr = p_new;
+			p = p_new;
+		}
+		++idx;
+
+		return start;
+	}
+
+	assert(0 && "malformed token list");
+	throw;
+}
+
 std::vector<p_datum> parse(token_list &tokens) {
 	std::vector<p_datum> tree;
 	size_t idx(0);
 	while (idx < tokens.size()) {
-		tree.push_back(tokens[idx]->parse(tokens, idx));
+		tree.push_back(parse_next(tokens, idx));
 	}
 	return tree;
 }
--- stranx/token.h
+++ stranx/token.h
@@ -5,60 +5,34 @@
 
 #include <deque>
 
-struct token;
+typedef std::deque<std::unique_ptr<object>> token_list;
 
-typedef std::deque<std::unique_ptr<token>> token_list;
-
-struct token : object {
-	virtual p_datum parse(const token_list &tokens, size_t &idx) const = 0;
-};
-
-struct begin_list : token {
-	operator std::string() const override {
-		return "(";
-	}
-
-	p_datum parse(const token_list &tokens, size_t &idx) const override;
-};
-
-struct end_list : token {
-	operator std::string() const override {
-		return ")";
-	}
-
-	p_datum parse(const token_list &, size_t &) const override {
-		assert(0 && "unexpected end of list token");
-		throw;
-	}
-};
-
-struct dot : token {
-	operator std::string() const override {
-		return ".";
-	}
-
-	p_datum parse(const token_list &, size_t &) const override {
-		assert(0 && "unexpected dot token");
-		throw;
-	}
-};
-
-struct atom : token {
+struct atom : object {
 	atom(const p_datum &p_d) : val(p_d) {}
 
 	operator std::string() const override {
 		return *val;
 	}
 
-	p_datum parse(const token_list &tokens, size_t &idx) const override {
-		assert(tokens.at(idx).get() == this && "first token in tokens is not current token");
-
-		++idx;
-		return val;
-	}
-
-private:
 	const p_datum val;
+};
+
+struct begin_list : object {
+	operator std::string() const override {
+		return "(";
+	}
+};
+
+struct end_list : object {
+	operator std::string() const override {
+		return ")";
+	}
+};
+
+struct dot : object {
+	operator std::string() const override {
+		return ".";
+	}
 };
 
 #endif
