b76964d84d9ddd6c065f6b64eee9b9107fdb8272
9t8 <63334933+9t8@users.noreply.github.com>
2022-12-25 03:28:09 -0800

    greatly reduced build times

--- stranx/procedure.cpp
+++ stranx/procedure.cpp
@@ -48,3 +48,68 @@ const_p_env procedure::make_new_env(const p_datum &args) const {
 	}
 	return new_env;
 }
+
+p_datum lambda::call(const p_datum &args, const_p_env &env) const {
+	const datum &temp(*args);
+	assert(typeid(temp) == typeid(pair) && "malformed argument list (not enough arguments?)");
+	const pair &args_list(dynamic_cast<const pair &>(temp));
+
+	std::vector<std::string> formals;
+
+	std::shared_ptr<identifier> variadic_iden(
+		std::dynamic_pointer_cast<identifier>(args_list.car));
+	p_pair curr_formal(std::dynamic_pointer_cast<pair>(args_list.car));
+	while (curr_formal) {
+		variadic_iden = std::dynamic_pointer_cast<identifier>(curr_formal->cdr);
+
+		const datum &formal_iden(*next(curr_formal));
+		assert(typeid(formal_iden) == typeid(identifier) &&
+			   "all formals must be identifiers (variadics are not supported)");
+
+		formals.push_back(dynamic_cast<const identifier &>(formal_iden).name);
+	}
+	if (variadic_iden) {
+		formals.push_back(variadic_iden->name);
+	}
+
+	const p_pair body(std::dynamic_pointer_cast<pair>(args_list.cdr));
+	assert(body && "invalid procedure body");
+
+	return std::make_shared<procedure>(formals, variadic_iden.get(), body, env);
+}
+
+p_datum define::call(const p_datum &args, const_p_env &env) const {
+	const datum &temp(*args);
+	assert(typeid(temp) == typeid(pair) && "malformed argument list (not enough arguments?)");
+	const pair &args_list(dynamic_cast<const pair &>(temp));
+
+	const datum &temp1(*args_list.cdr);
+	assert(typeid(temp1) == typeid(pair) && "malformed argument list (not enough arguments?)");
+	const pair &cdr(dynamic_cast<const pair &>(temp1));
+
+	const datum &car(*args_list.car);
+
+	if (typeid(car) == typeid(identifier)) {
+		const datum &cddr(*cdr.cdr);
+		assert(typeid(cddr) == typeid(empty_list) && "too many arguments");
+
+		env->define(dynamic_cast<const identifier &>(*args_list.car).name, cdr.car->eval(env));
+	} else {
+		assert(typeid(car) == typeid(pair) && "first argument must be identifier or list");
+
+		const pair &formals(dynamic_cast<const pair &>(*args_list.car));
+
+		const datum &caar(*formals.car);
+		assert(typeid(caar) == typeid(identifier) && "procedure name must be an identifier");
+
+		const p_datum lambda(env->find("lambda"));
+		assert(dynamic_cast<const function *>(lambda.get()) &&
+			   "lambda was redefined into an uncallable object");
+
+		env->define(dynamic_cast<const identifier &>(*formals.car).name,
+					dynamic_cast<const function &>(*lambda).call(std::make_shared<pair>(
+								formals.cdr, args_list.cdr), env));
+	}
+
+	return std::make_shared<empty_list>();
+}
--- stranx/procedure.h
+++ stranx/procedure.h
@@ -32,21 +32,24 @@ private:
 	const_p_env env;
 };
 
-template <class func> struct native_function : function {
-	native_function(const func &cf) : call_func(cf) {}
-
+struct lambda : function {
 	operator std::string() const override {
 		std::ostringstream oss;
-		oss << "#native_function@" << this;
+		oss << "#lambda@" << this;
 		return oss.str();
 	}
 
-	p_datum call(const p_datum &args, const_p_env &env) const override {
-		return call_func(args, env);
+	p_datum call(const p_datum &args, const_p_env &env) const override;
+};
+
+struct define : function {
+	operator std::string() const override {
+		std::ostringstream oss;
+		oss << "#define@" << this;
+		return oss.str();
 	}
 
-private:
-	const func call_func;
+	p_datum call(const p_datum &args, const_p_env &env) const override;
 };
 
 #endif
