1f655ae7b6f94cf78a9fa2ac77fc163a108ca173
9t8 <63334933+9t8@users.noreply.github.com>
2023-01-06 07:43:23 -0800

    asdf

--- tests.scm
+++ tests.scm
@@ -78,16 +78,12 @@ x
 y
 
 ; true/false
-(define if (lambda (c t f) (c t f)))
-(define t (lambda (l r) l))
-(define f (lambda (l r) r))
-(if t 'a 'b)
-(if f 'c 'd)
-
-; swapped names
-(define (if c f t) (c f t))
+(define (if c t f) (c t f))
 (define (t l r) l)
 (define (f l r) r)
+(if t 'a 'b)
+(if f 'c 'd)
+(define (if c f t) (c f t))
 (if t 'a 'b)
 (if f 'c 'd)
 
